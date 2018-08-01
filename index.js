77f0ec0ebe1d0032b4843ddbac27cb4aecf950d1
Mark Semotiuk <marksem@gmail.com>
2018-08-01 03:17:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-var price = Math.floor(Math.random() * 100)
+var price = Math.ceil(Math.random() * 100)
 var newcartitem = {itemName: item, itemPrice: price}
 cart.unshift(newcartitem)
 return `${item} has been added to your cart.`
